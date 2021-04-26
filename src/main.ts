4f6ed99b6d9e06eae94319e5fb0b41bc268b5356
Toc <tocwebtoc@gmail.com>
2021-04-26 09:49:41 +0200

    Readonly / public / protected / private in TypeScript Classes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 class User {
-    firstName: string
-    lastName: string
+    private firstName: string
+    private lastName: string
+    readonly unchangableName: string
 
     constructor(firstName: string, lastName: string) {
         this.firstName = firstName
         this.lastName = lastName
+        this.unchangableName = firstName;
+    }
+
+    changeUnchagableName(): void {
+        // Cant be asign cause is a read only property
+        // this.unchangableName ="foo";
     }
 
     getFullName() :string {
@@ -13,4 +20,5 @@ class User {
 }
 
 const user = new User('Toc', 'Dev');
+// The Only properties available for user is getFullName
 console.log(user.getFullName);
