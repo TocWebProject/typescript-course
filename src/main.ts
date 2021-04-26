f1fb9104b20f4e92a186b72c1adb1c6ba57eefc0
Toc <tocwebtoc@gmail.com>
2021-04-26 08:48:52 +0200

    Another exempler for Type Assertion

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 let vAny: any = 10;
 let vUnknown: unknown = 10;
 
+// "as" operator makes type assertion
 let s1: string = vAny;
 let s2: string = vUnknown as string;
+
+let pageNumber: string = "1";
+let numericPageNumber: number = (pageNumber as unknown) as number;
